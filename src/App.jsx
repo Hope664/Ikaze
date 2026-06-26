83d1d98f95b861c33053b6ec7011a8bec6fbb6ba
hope664 <umuhozahope5@gmail.com>
2026-06-26 14:46:56 +0200

    finalized quiz section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import VendorDetail from './pages/VendorDetail'
 import Discover from './pages/Discover'
 import Auth from './pages/Auth'
+import Quiz from '/pages/Quiz'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/vendor/:id" element={<VendorDetail />} />
       <Route path="/discover" element={<Discover />} />
       <Route path="/auth" element={<Auth />} />
+      <Route path="/quiz" element={<Quiz />} />
     </Routes>
   )
 }
