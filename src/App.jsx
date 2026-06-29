0d3e1b5f2b0566874fb5c8cf4a36ad8136c7271b
hope664 <umuhozahope5@gmail.com>
2026-06-29 23:18:59 +0200

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-
-// Public pages - visible to everyone
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Quiz from './pages/user/Quiz'
 import Discover from './pages/user/Discover'
 import VendorDetail from './pages/user/VendorDetail'
-
-// User pages - only for logged in users
+import Categories from './pages/user/Categories'
+import HowItWorks from './pages/user/HowItWorks'
 import UserDashboard from './pages/user/UserDashboard'
 import Booking from './pages/user/Booking'
 import BookingConfirmed from './pages/user/BookingConfirmed'
-
 import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
@@ -23,6 +20,8 @@ function App() {
       <Route path="/quiz" element={<Quiz />} />
       <Route path="/discover" element={<Discover />} />
       <Route path="/vendor/:id" element={<VendorDetail />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/how-it-works" element={<HowItWorks />} />
 
       {/* ===== USER - must be logged in ===== */}
       <Route path="/user/dashboard" element={
